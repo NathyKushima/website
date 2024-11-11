136f42e97be7347ead19dd8a878f91a6c642d4c9
NathyKushima <nhkushima@gmail.com>
2024-11-10 22:55:00 -0300

    Insert listing, searching and creating movies

--- based/settings.py
+++ based/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'staticpages.apps.StaticpagesConfig',
+    'categories.apps.CategoriesConfig',
 ]
 
 MIDDLEWARE = [
--- based/urls.py
+++ based/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('staticpages.urls')),
+    path('categories/', include('categories.urls')),
     path('admin/', admin.site.urls),
 ]
